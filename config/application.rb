66e3a56468c27bfab3e578b84d0652278da147b8
Jace Bayless <jacebayless@gmail.com>
2021-03-22 19:39:18 +0000

    Deploy 3

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module EECS647Project
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    logger = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 end
