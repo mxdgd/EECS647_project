223a3a6f239349f226370b87bfe0f4d1ed67ae48
Tri Pham <thientri399@gmail.com>
2021-04-08 06:29:05 +0000

    Added film show and index

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,9 +1,9 @@
 class FilmsController < ApplicationController
   def index
-
+    @films = Film.all().order(release_date: :desc)
   end
   
   def show
-
+    @film = Film.find(params[:id])
   end
 end
