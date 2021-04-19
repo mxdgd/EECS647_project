49dc07c5e872e68d00cbc5b72fdfa03f10fc59d8
Jace Bayless <jacebayless@gmail.com>
2021-04-19 06:36:44 +0000

    Not displaying pagination when only one page

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,7 +1,7 @@
 class FilmsController < ApplicationController
   def index
     @page = params.fetch(:page, 0).to_i
-    @pages_count = (Film.all.count / 5).ceil
+    @pages_count = (Film.all.count / 5.to_f).ceil
     @films = Film.offset(@page*5).limit(5).order(release_date: :desc)
   end
   
