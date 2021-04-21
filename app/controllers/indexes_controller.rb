8b26cec22183740283327f5861d56ba29b662471
Jace Bayless <jacebayless@gmail.com>
2021-04-21 04:46:45 +0000

    Small bug fixes

--- app/controllers/indexes_controller.rb
+++ app/controllers/indexes_controller.rb
@@ -3,7 +3,7 @@ class IndexesController < ApplicationController
     @about = About.first()
     @films = Film.all().order(release_date: :desc)
     @contact = Contact.first()
-    @films_with_awards = Film.joins(:awards).distinct
-    @small_team_films = Film.joins("INNER JOIN cast_members ON films.id = cast_members.film_id GROUP BY films.id HAVING COUNT(*) < 6")
+    @films_with_awards = Film.joins(:awards).order(release_date: :desc).distinct
+    @small_team_films = Film.joins("INNER JOIN cast_members ON films.id = cast_members.film_id GROUP BY films.id HAVING COUNT(*) < 6").order(release_date: :desc)
   end
 end
